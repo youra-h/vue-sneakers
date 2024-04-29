35408e8f00793eddff19d933b4ab27ad27a242c7
Youra <bestletter@inbox.ru>
2024-04-29 11:07:49 +0500

    update screensaver

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,10 +1,23 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+const images: Array<string> = ['/screensaver1.png', '/screensaver2.png', '/screensaver3.png'];
+
+const randomImage = computed<string>(() => images[Math.floor(Math.random() * images.length)]);
+</script>
+
 <template>
     <div class="grid grid-cols-2 h-screen">
         <div>
             <slot></slot>
         </div>
-        <div class="bg-slate-700 flex justify-center items-center">
-            <img width="600" src="/screensaver.png" alt="Auth background" class="object-cover" />
+        <div class="bg-slate-700 flex justify-center items-center object-cover"
+            :style="{ backgroundImage: 'url(' + randomImage + ')' }">
         </div>
     </div>
 </template>
+<style scoped>
+.object-cover {
+    background: url('/screensaver3.png') repeat repeat;
+    background-size: 70% 70%;
+}
+</style>
